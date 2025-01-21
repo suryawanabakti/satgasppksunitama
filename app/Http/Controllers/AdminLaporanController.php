90d01c1e5a98d4063ca8228b87b6dccab7712354
surya <suryawanabakti18@gmail.com>
2025-01-21 15:55:26 +0800

    TEST

--- app/Http/Controllers/AdminLaporanController.php
+++ app/Http/Controllers/AdminLaporanController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\AdminLaporanResoruce;
+use App\Mail\LaporanMail;
 use App\Models\Laporan;
 use App\Services\Fonnte;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Inertia\Inertia;
 
 class AdminLaporanController extends Controller
@@ -18,18 +20,21 @@ class AdminLaporanController extends Controller
 
     public function destroy(Laporan $laporan)
     {
+        Mail::to($laporan->user->username)->send(new LaporanMail("Laporan Di Hapus"));
         $laporan->delete();
         return back();
     }
 
     public function terima(Laporan $laporan)
     {
-        Fonnte::sendWa("085183028432", "Laporan di setujui");
+        // Fonnte::sendWa("085183028432", "Laporan di setujui");
+        Mail::to($laporan->user->username)->send(new LaporanMail("Laporan Diterima"));
         $laporan->update(['status' => 'DISETUJUI']);
         return back();
     }
     public function tolak(Laporan $laporan)
     {
+        Mail::to($laporan->user->username)->send(new LaporanMail("Laporan Di Tolak"));
         $laporan->update(['status' => 'DITOLAK']);
         return back();
     }
