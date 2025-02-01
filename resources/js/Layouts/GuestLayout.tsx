6ee50d3fe3e3a3641285df50cd1c54fa7d8ae61d
surya <suryawanabakti18@gmail.com>
2025-02-02 00:49:07 +0800

    TEST

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -11,11 +11,12 @@ export default function Guest({ children }: PropsWithChildren) {
                         src="/lambang.png"
                         className="h-20 w-20 fill-current text-gray-500"
                     />
-                    {/* <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" /> */}
                 </Link>
             </div>
-
-            <div className="mt-6 w-full overflow-hidden  px-6 py-4  sm:max-w-md sm:rounded-lg">
+            <h4 className="text-xl text-center mt-3 font-bold">
+                SATGAS PPKS UNITAMA
+            </h4>
+            <div className="mt-6 w-full overflow-hidden  px-6 py-1  sm:max-w-md sm:rounded-lg">
                 {children}
             </div>
         </div>
